c4508eaf9e34599a88944e80c6b8958166da7d7b
marcospedro97 <marcos.pedro.nunes@gmail.com>
2020-11-12 20:20:08 -0300

    Tests user close task DONE

---
+++ spec/features/user_close_task_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'user close task' do
+  scenario 'successfully', js: true do
+    # ARRANGE
+    user = create(:user)
+    task_list = create(:task_list, user: user)
+    tasks = create_list(:task, 5, task_list_id: task_list.id)
+    # ACT
+    login_as user, scope: :user
+    visit root_path
+    click_on 'Minhas listas'
+    click_on task_list.title
+    first_task = all('.task').first
+    within(first_task) do
+      click_on 'Fechar tarefa'
+    end
+    # ASSERT
+    expect(Task.where(task_list_id: task_list.id, open: false).count).to eq(1)
+  end
+end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should not close from other user' do
+    task_list = create(:task_list_with_tasks)
+    user = create(:user)
+    task_list.tasks.first.close(user.id)
+    expect(task_list.tasks.first.open).to eq(true)
+  end
 end
