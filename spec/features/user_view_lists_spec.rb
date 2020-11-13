84f26e7530c27acb713b3cd472aaa01a9d78681d
marcospedro97 <marcos.pedro.nunes@gmail.com>
2020-11-12 21:22:23 -0300

    Test user view tasks DONE

---
+++ spec/features/user_view_lists_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+describe 'user view lists' do
+  context 'public' do
+    it 'successfully' do
+      # ARRANGE
+      user = create(:user)
+      create_list(:task_list, 5, public: true)
+      create_list(:task_list, 6, public: false)
+      # ACT
+      login_as user, scope: :user
+      visit root_path
+      click_on 'Listas públicas'
+      # ASSERT
+      expect(all('.list').count).to eq(5)
+    end
+
+    it 'should be authenticated' do
+      # ARRANGE
+      create_list(:task_list, 5, public: true)
+      # ACT
+      visit public_task_lists_path
+      # ASSERT
+      expect(current_path).to eq(new_user_session_path)
+    end
+  end
+
+  context 'private' do
+    it 'successfully' do
+      # ARRANGE
+      user = create(:user)
+      create_list(:task_list, 5, user_id: user.id, public: false)
+      create_list(:task_list, 6, public: true)
+      # ACT
+      login_as user, scope: :user
+      visit root_path
+      click_on 'Minhas listas'
+      # ASSERT
+      expect(all('.list').count).to eq(5)
+    end
+
+    it 'should be authenticated' do
+      # ARRANGE
+      create_list(:task_list, 5, public: true)
+      # ACT
+      visit user_task_lists_path
+      # ASSERT
+      expect(current_path).to eq(new_user_session_path)
+    end
+  end
+end
